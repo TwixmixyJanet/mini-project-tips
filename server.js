1e586ae72341f21e13c80a0e39e463f15ce77bd6
Fiqre-Ab <Fiqreyesusabraham@gmail.com>
2023-10-26 15:10:36 -0500

    post

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 const diagnostics = require('./db/diagnostics.json');
+const fs = require('fs');
+const { json } = require('body-parser');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001; // Change "port" to "PORT"
 
 const app = express();
 
@@ -20,28 +22,45 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+  res.sendFile(path.join(__dirname, '/public/index.html')
+));
 
 // GET Route for feedback page
 app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html')
+));
 
 // GET Route for API Diagnostics
-app.post('/api/diagnostics', (req, res) => {
-  return res.json(diagnostics)
-});
-
-// GET Route for wildcard
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/pages/404.html"));
+app.get('/api/diagnostics', (req, res) => {
+  return res.json(diagnostics);
 });
 
 // POST Route
 app.post('/api/diagnostics', (req, res) => {
   res.json(`${req.method} request received`);
   // Needs to store information about the invalid form submission
+  const requestData = req.body;
+  fs.readFile('db/diagnostics.json', 'utf8', (err, data) => {
+    if (err) {
+      data = []; // Initialize data as an empty array if the file doesn't exist
+    } else {
+      data = JSON.parse(data); // Use JSON.parse to parse the existing data
+    }
+    data.push(requestData);
+    fs.writeFile('db/diagnostics.json', JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error logging data');
+      } else {
+        res.send('Data logged successfully');
+      }
+    });
+  });
+});
+
+// GET Route for wildcard
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'./public/pages/404.html'));
 });
 
 app.listen(PORT, () =>
